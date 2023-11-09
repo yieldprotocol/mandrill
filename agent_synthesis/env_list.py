30688ffc68775e70532de262f8ca54762084af64
harshraj172 <harsh777111raj@gmail.com>
2023-11-10 00:49:10 +0530

    added: pipeline for agent data gen

---
+++ agent_synthesis/env_list.py
@@ -0,0 +1,129 @@
+environments = [
+    # Refinery & Manufacturing Systems
+    "Refinery Control Simulator: The agent monitors and adjusts virtual refinery operations to ensure safe and optimal production. Technicians can calibrate equipment and perform diagnostics.",
+    "Factory Floor Management: The agent schedules, monitors, and optimizes manufacturing processes, ensuring timely production. Technicians can maintain and calibrate machinery.",
+    "CNC Machine Controller: The agent designs and executes CNC machining processes based on design inputs. Machine technicians can calibrate and troubleshoot the machine.",
+    "Automated Assembly Line: The agent manages an assembly line's pace, worker assignments, and quality checks. Technicians can perform maintenance checks and repairs.",
+
+    # Scientific & Engineering Software
+    "Petrel Reservoir Simulation: The agent manages and analyzes virtual oil reservoir simulations, optimizing extraction strategies. Geoscientists can adjust parameters and analyze geological data.",
+    "GROMACS Molecular Dynamics: The agent runs molecular simulations, analyzing protein structures and interactions. Researchers can adjust simulation parameters and analyze results.",
+    "ANSYS Fluent Flow Simulator: The agent sets up and runs fluid dynamics simulations, analyzing airflow over virtual objects. Engineers can adjust meshing and boundary conditions.",
+    "MCNP Particle Simulation: The agent sets up and monitors nuclear particle simulations, evaluating radiation doses and shielding effectiveness. Nuclear scientists can adjust simulation parameters.",
+    "Kingdom Suite Seismic Analysis: The agent processes and interprets seismic data for oil and gas exploration. Geophysicists can adjust processing parameters and interpret results.",
+    "SPICE Circuit Simulation: The agent designs and simulates electronic circuits, analyzing their behavior under various conditions. Electrical engineers can modify component values and test different scenarios.",
+    "NAPA Ship Design: The agent designs virtual ship hulls and evaluates their hydrodynamic performance. Naval architects can modify design parameters and run simulations.",
+    "OpenTrack Railway Operations: The agent schedules and manages virtual railway operations, optimizing for efficiency and safety. Railway technicians can maintain tracks and signals.",
+    "KDB+/q Data Analysis: The agent queries and processes large datasets in real-time, extracting insights and patterns. Data analysts can adjust query parameters and visualize results.",
+    "Cadence Virtuoso IC Design: The agent designs integrated circuits and tests their functionality. Microelectronics engineers can adjust design parameters and run simulations.",
+    "CST Studio Electromagnetic Analysis: The agent sets up and runs electromagnetic simulations, evaluating antenna designs and electromagnetic interference. RF engineers can adjust simulation parameters and evaluate results.",
+    "TunnelCAD Tunnel Design: The agent designs virtual tunnels, evaluating their stability and safety. Civil engineers can adjust design parameters and run simulations.",
+
+    # Miscellaneous Domains
+    "DNA Sequencing Simulator: The agent processes and interprets virtual DNA samples, identifying genes and mutations. Biologists can adjust sequencing parameters and analyze results.",
+    "Virtual Architectural Planner: The agent designs building layouts, evaluating structural integrity and aesthetics. Architects can modify design elements and run simulations.",
+    "Financial Forecasting System: The agent analyzes financial data to predict market trends. Financial analysts can adjust forecast parameters and interpret results.",
+    "Urban Traffic Management: The agent manages traffic lights and flow in a virtual city, optimizing for reduced congestion. Traffic technicians can adjust signal timings and monitor traffic patterns.",
+    "Drone Flight Controller: The agent plans and executes drone flight paths, ensuring safe and efficient operations. Drone technicians can calibrate and troubleshoot drone components.",
+    "Virtual Retail Store Manager: The agent manages inventory, sales, and customer interactions in a virtual retail environment. Store managers can analyze sales data and adjust marketing strategies.",
+    "Automated Journalism Simulator: The agent gathers data and composes news articles or reports. Editors can adjust article parameters and approve final drafts.",
+    "Text-based Marine Navigator: The agent plans and navigates maritime routes, avoiding obstacles and optimizing for fuel efficiency. Ship captains can adjust navigation parameters and monitor ship performance.",
+    "Virtual Astronomy Observatory: The agent analyzes data from virtual telescopes, identifying celestial objects and phenomena. Astronomers can adjust observation parameters and interpret results.",
+    "Hydroelectric Power Plant Simulator: The agent manages operations in a virtual hydroelectric power plant, optimizing for energy production. Engineers can calibrate turbines and monitor water levels.",
+    "Text-based Theme Park Manager: The agent designs and manages a virtual theme park, ensuring guest satisfaction and safety. Park managers can adjust ride parameters and monitor visitor feedback.",
+    "Automated Legal Advisor: The agent analyzes legal texts and provides advice or interpretations. Lawyers can adjust query parameters and review interpretations.",
+    "Environmental Impact Analyzer: The agent evaluates the environmental impact of various projects or decisions based on input data. Environmentalists can adjust evaluation parameters and interpret results.",
+    "Virtual Wildlife Conservationist: The agent monitors and manages virtual wildlife populations, ensuring their survival and health. Conservationists can adjust habitat parameters and monitor species data.",
+    "Smart Grid Energy Manager: The agent optimizes energy distribution in a virtual smart grid, balancing supply and demand. Technicians can calibrate grid components and monitor energy flows.",
+    "Text-based Astronaut Trainer: The agent trains and prepares for virtual space missions, ensuring readiness for real-life space exploration. Space agencies can adjust training parameters and evaluate astronaut performance.",
+    "Virtual Art Auctioneer: The agent evaluates and auctions virtual art pieces, optimizing for profitability and buyer satisfaction. Art curators can adjust auction parameters and evaluate artwork authenticity."
+]
+
+original_environments = [
+    # Bookings & Reservations
+    "Booking Reservation System: The agent interacts with a simulated booking system to reserve flights, hotels, or restaurants based on criteria. Travel agents can adjust reservation details and monitor booking statuses.",
+
+    # Libraries & Databases
+    "Library Database Manager: The agent queries a text-based library system to find books or articles based on topics or authors. Librarians can update records, analyze usage, and maintain the system.",
+
+    # Technical Support
+    "Tech Support Simulator: The agent engages with a virtual user reporting technical problems, providing troubleshooting steps. Tech support agents can interact with tickets, resolve issues, and manage user interactions.",
+
+    # Financial & Stock Market
+    "Virtual Stock Market Analyst: The agent receives textual data on stocks, making buy/sell decisions based on information. Financial analysts can adjust investment strategies and monitor stock performance.",
+
+    # Event Planning
+    "Event Planner Assistant: The agent organizes events, manages schedules, and handles invitations considering constraints and preferences. Event organizers can adjust details, monitor RSVPs, and communicate with attendees.",
+
+    # Gaming & Narrative
+    "Text-based Game Environment: The agent engages in a story-driven game, making decisions that influence the narrative's outcome. Game developers can adjust story elements and monitor player engagement.",
+
+    # Culinary & Cooking
+    "Recipe Query System: The agent searches and proposes recipes based on available ingredients. Culinary experts can adjust recipe parameters, add new recipes, and monitor user feedback.",
+
+    # Museums & Exhibitions
+    "Virtual Museum Guide: The agent escorts a user through a text-based museum, explaining artworks or exhibits tailored to user interest. Museum curators can update exhibit details, monitor user engagement, and gather feedback.",
+
+    # Education & Learning
+    "E-Learning Platform Navigator: The agent navigates an online learning platform, enrolling in courses, and engaging in quizzes. Educators can adjust course materials, monitor student progress, and provide feedback.",
+
+    # Fitness & Health
+    "Text-based Fitness Trainer: The agent designs workout routines, monitors progress, and gives advice based on health and fitness inputs. Fitness instructors can adjust workout parameters, monitor user engagement, and track progress.",
+
+    # Job Interviews
+    "Automated Interview Simulator: The agent plays both interviewer and interviewee roles across various job domains, facilitating questions and answers. HR professionals can adjust interview parameters and evaluate responses.",
+
+    # Medical & Health
+    "Medical Diagnosis Assistant: The agent evaluates provided symptoms, gives potential diagnoses, and recommends next steps. Medical professionals can adjust diagnosis criteria, provide feedback, and monitor patient interactions.",
+
+    # Agriculture & Farming
+    "Text-based Farming Simulator: The agent oversees a virtual farm, making decisions about crops, livestock, and sales. Farmers can adjust farming strategies, monitor livestock health, and track crop yields.",
+
+    # Real Estate & Housing
+    "Virtual Real Estate Agent: The agent searches, lists, and suggests properties based on preferences and constraints. Real estate brokers can adjust property details, monitor client feedback, and handle transactions.",
+
+    # Software & Version Control
+    "Version Control System Operator: The agent interacts with a text-based version control system (like Git), handling commits, branches, merges, and conflict resolution. Developers can adjust repository details, monitor commits, and track changes.",
+    
+    # Software Development & CI/CD
+    "CI/CD Pipeline Manager: The agent oversees build pipelines, runs tests, and handles software deployment based on textual logs and commands. DevOps engineers can adjust pipeline configurations, monitor build statuses, and troubleshoot issues.",
+
+    # Cloud & Infrastructure
+    "Cloud Infrastructure Manager: The agent interfaces with a simulated cloud service to provision servers, manage databases, and allocate resources. Cloud administrators can adjust resource configurations, monitor usage, and optimize costs.",
+
+    # Code Review & Optimization
+    "Automated Code Reviewer: The agent reviews code snippets, provides feedback, suggests enhancements, and identifies potential issues. Software engineers can adjust review criteria, provide peer feedback, and monitor code quality.",
+
+    # Algorithm Design & Testing
+    "Algorithm Design Simulator: The agent is given problems, designs solutions, tests algorithms, and evaluates their performance. Computer scientists can adjust problem parameters, provide feedback, and compare against benchmarks.",
+
+    # Databases & Data Management
+    "Database Design & Management System: The agent creates database schemas, normalizes data structures, and fine-tunes queries. Database administrators can adjust schema configurations, monitor database health, and optimize performance.",
+
+    # Networking & Connectivity
+    "Network Management Simulator: The agent configures and manages virtual networks, setting up routers, switches, and ensuring connectivity. Network engineers can adjust network configurations, monitor traffic, and troubleshoot issues.",
+
+    # Embedded Systems & Firmware
+    "Embedded Systems Designer: The agent programs and tests firmware for simulated embedded devices, ensuring correct functionality. Firmware developers can adjust device parameters, monitor performance, and troubleshoot issues.",
+
+    # API Design & Interaction
+    "API Design & Interaction Simulator: The agent creates, tests, and communicates with simulated RESTful or GraphQL APIs, ensuring valid endpoints and responses. Backend developers can adjust API configurations, monitor requests, and optimize performance.",
+
+    # Web Development & Design
+    "Web Development Simulator: The agent receives a design brief and crafts HTML, CSS, and JavaScript to render a functional webpage or app. Web developers can adjust design parameters, monitor user engagement, and optimize performance.",
+
+    # Compilers & Interpreters
+    "Compiler Design Simulator: The agent crafts and tests code for a virtual compiler or interpreter, ensuring proper code translation. Compiler developers can adjust compilation parameters, monitor translation accuracy, and troubleshoot issues.",
+
+    # Operating Systems & Scheduling
+    "Operating System Scheduler Simulator: The agent manages virtual OS processes, handles memory allocation, and schedules tasks. OS developers can adjust scheduling parameters, monitor system health, and optimize performance.",
+
+    # Software Testing & Quality Assurance
+    "Automated Testing Environment: The agent authors unit, integration, and end-to-end tests for software modules, and assesses test outcomes. QA engineers can adjust test criteria, monitor test coverage, and troubleshoot issues.",
+
+    # Security & Threat Management
+    "Security Breach Simulator: The agent detects and responds to simulated security threats in a virtual ecosystem, mitigating risks. Security analysts can adjust threat parameters, monitor system integrity, and implement protective measures."
+]
+
+all_environments_description = original_environments + environments 
+
